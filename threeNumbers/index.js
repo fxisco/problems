b39190846184d279f414f776b7f2b8ed7be5f0eb
Francisco Payes <payesfrancisco@gmail.com>
2019-01-07 16:41:51 -0500

    (ThreeNumbers) initial commit

---
+++ threeNumbers/index.js
@@ -0,0 +1,30 @@
+/**
+ * Challenge
+ *
+ * Have the function ThreeNumbers(str) take the str parameter being
+ * passed and determine if exactly three unique, single-digit integers
+ * occur within each word in the string. The integers can appear anywhere
+ * in the word, but they cannot be all adjacent to each other.
+ *
+ * If every word contains exactly 3 unique integers somewhere within it,
+ * then return the string true, otherwise return the string false.
+ * For example: if str is "2hell6o3 wor6l7d2" then your program should
+ * return "true" but if the string is "hell268o w6or2l4d" then your program
+ * should return "false" because all the integers are adjacent to each other
+ * in the first word.
+ */
+
+function ThreeNumbers(string) {
+  const words = string.split(' ');
+
+  return words;
+}
+
+console.log(ThreeNumbers("2a3b5 w1o2rl3d g1gg92"));
+
+
+// Input:"2a3b5 w1o2rl3d g1gg92"
+// Output:"true"
+
+// Input:"21aa3a ggg4g4g6ggg"
+// Output:"false"
