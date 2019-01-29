bf5e47b8c10773ec194a8ad8a06db16653190cb3
Francisco Payés <payesfrancisco@gmail.com>
2019-01-29 19:32:43 -0400

    (Camel Case): solution

--- camelCase/index.js
+++ camelCase/index.js
@@ -1,16 +1,29 @@
 /**
  * Challenge
-Have the function CamelCase(str) take the str parameter being passed and return it in proper camel case format where the first letter of each word is capitalized (excluding the first letter). The string will only contain letters and some combination of delimiter punctuation characters separating each word.
-
-For example: if str is "BOB loves-coding" then your program should return the string bobLovesCoding.
+ *
+ * Have the function CamelCase(str) take the str parameter being passed and return it in proper camel case
+ * format where the first letter of each word is capitalized (excluding the first letter). The string will
+ * only contain letters and some combination of delimiter punctuation characters separating each word.
+ *
+ * For example: if str is "BOB loves-coding" then your program should return the string bobLovesCoding.
  */
 
+function CamelCase(str) {
+  const sanitaizedStr = str.toLowerCase().replace(/[^a-z]/g, ' ');
+
+  return [...sanitaizedStr.split(' ')].reduce((accum, item, index) => {
+    if (index > 0) {
+      return accum + item.charAt(0).toUpperCase() + item.slice(1);
+    }
+
+    return accum +  item;
+  }, '');
+}
+
+console.log(CamelCase('a b c d-e-f%g'));
 
 // Input:"cats AND*Dogs-are Awesome"
-
 // Output:"catsAndDogsAreAwesome"
 
-
 // Input:"a b c d-e-f%g"
-
 // Output:"aBCDEFG"
