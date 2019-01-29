dc6ae36c52ae6813842c190ad9c6737362bcf624
Francisco Payés <payesfrancisco@gmail.com>
2019-01-29 12:55:13 -0400

    (Camel Case): create project

---
+++ camelCase/index.js
@@ -0,0 +1,16 @@
+/**
+ * Challenge
+Have the function CamelCase(str) take the str parameter being passed and return it in proper camel case format where the first letter of each word is capitalized (excluding the first letter). The string will only contain letters and some combination of delimiter punctuation characters separating each word.
+
+For example: if str is "BOB loves-coding" then your program should return the string bobLovesCoding.
+ */
+
+
+// Input:"cats AND*Dogs-are Awesome"
+
+// Output:"catsAndDogsAreAwesome"
+
+
+// Input:"a b c d-e-f%g"
+
+// Output:"aBCDEFG"
