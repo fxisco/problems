8ebb9bec06fc5e9152301a390595e6028c6a3738
Francisco Payés <payesfrancisco@gmail.com>
2019-01-30 23:14:38 -0400

    (Simple Evens): init project

---
+++ simpleEvens/index.js
@@ -0,0 +1,20 @@
+/**
+ * Challenge
+ * Have the function SimpleEvens(num) check whether every single number in the passed
+ * in parameter is even. If so, return the string true, otherwise return the string false.
+ *
+ * For example: if num is 4602225 your program should return the string false because 5
+ * is not an even number.
+ */
+
+function SimpleEvens(num) {
+  return num;
+}
+
+console.log(SimpleEvens(4602225));
+
+ // Input:2222220222
+// Output:"true"
+
+// Input:20864646452
+// Output:"false"
