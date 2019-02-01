e6f8367970150c97ef5af11d5c6fba92d8b0cf17
Francisco Payés <payesfrancisco@gmail.com>
2019-01-31 22:26:09 -0400

    (Simple Evens): solution

--- simpleEvens/index.js
+++ simpleEvens/index.js
@@ -8,12 +8,25 @@
  */
 
 function SimpleEvens(num) {
-  return num;
+  let allEvens = true;
+
+  while (num > 0) {
+    const module = num % 10;
+
+    if (module % 2 !== 0) {
+      allEvens = false;
+      break;
+    } else {
+      num = Math.trunc(num / 10);
+    }
+  }
+
+  return allEvens;
 }
 
-console.log(SimpleEvens(4602225));
+console.log(SimpleEvens(20864646452));
 
- // Input:2222220222
+// Input:2222220222
 // Output:"true"
 
 // Input:20864646452
