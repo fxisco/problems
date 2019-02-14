f521a2e973dd1182fafb876ac9718e2bb56ea6fc
Francisco Payés <payesfrancisco@gmail.com>
2019-02-14 00:16:09 -0400

    (One Decremented): init project

---
+++ oneDecremented/index.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge
+ * Have the function OneDecremented(num) count how many times a digit appears that is
+ * exactly one less than the previous digit.
+ *
+ * For example: if num is 5655984 then your program should return 2 because 5 appears
+ * directly after 6 and 8 appears directly after 9.
+ *
+ * The input will always contain at least 1 digit.
+ */
+
+function OneDecremented(num) {
+  return num;
+}
+
+console.log(OneDecremented(5655984));
+
+// Input:56
+// Output:0
+
+// Input:9876541110
+// Output:6
+
