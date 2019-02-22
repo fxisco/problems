f6afc444dc25fa3c2893b937f9dbd7296890e03a
Francisco Payés <payesfrancisco@gmail.com>
2019-02-22 01:31:16 -0400

    (String Periods): init project

---
+++ stringPeriods/index.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge
+ * Have the function StringPeriods(str) take the str parameter being passed and determine
+ * if there is some substring K that can be repeated N > 1 times to produce the input string
+ * exactly as it appears. Your program should return the longest substring K, and if there
+ * is none it should return the string -1.
+ *
+ * For example: if str is "abcababcababcab" then your program should return abcab because that
+ * is the longest substring that is repeated 3 times to create the final string. Another example:
+ * if str is "abababababab" then your program should return ababab because it is the longest substring.
+ * If the input string contains only a single character, your program should return the string -1.
+ */
+
+function StringPeriods(str) {
+  return str;
+}
+
+// Input:"abcxabc"
+// Output:"-1"
+
+
+// Input:"affedaaffed"
+// Output:"-1"
