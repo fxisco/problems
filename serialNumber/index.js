672e1be9f49f03e72a5ea1e82f86927706820e81
Francisco Payés <payesfrancisco@gmail.com>
2019-02-22 01:27:54 -0400

    (Serial Number): solution

--- serialNumber/index.js
+++ serialNumber/index.js
@@ -14,12 +14,48 @@
  * For example: if str is "224.315.218" then your program should return "true".
  */
 
+function sum(accum, item) {
+  accum += parseInt(item);
 
-function SerialNumber(str) {
-  return str;
+  return accum;
 }
 
-console.log(SerialNumber('224.315.218'));
+function isSetAddToEvenNumber(set) {
+  const numbers = set.split('');
+
+  return numbers.reduce(sum, 0) % 2 === 0;
+}
+
+function isSetAddToOddNumber(set) {
+  const numbers = set.split('');
+
+  return numbers.reduce(sum, 0) % 2 !== 0;
+}
+
+function isLastDigitEachSetLargerThanTwoPreviousDigits(sets) {
+  return !(sets.some(set => {
+    const setLastValue = set[set.length - 1];
+
+    return parseInt(set.split('').sort()[0]) > parseInt(setLastValue);
+  }));
+}
+
+function SerialNumber(str) {
+  let isValid = false;
+  let hasThreeSetsWithThreeDigits = str.match(/[1-9]{3}.[1-9]{3}.[1-9]{3}/);
+
+  if (hasThreeSetsWithThreeDigits) {
+    let sets = str.split('.');
+
+    isValid = isSetAddToEvenNumber(sets[0]) &&
+      isSetAddToOddNumber(sets[1]) &&
+      isLastDigitEachSetLargerThanTwoPreviousDigits(sets);
+  }
+
+  return isValid;
+}
+
+console.log(SerialNumber('114.568.112'));
 
 // Input:"11.124.667"
 // Output:"false"
