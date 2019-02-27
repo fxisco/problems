7dd18ca34384f3cb159d4023990253b3a9777b4e
Francisco Payés <payesfrancisco@gmail.com>
2019-02-27 00:12:50 -0400

    (String Periods): solution

--- stringPeriods/index.js
+++ stringPeriods/index.js
@@ -12,8 +12,38 @@
  */
 
 function StringPeriods(str) {
-  return str;
+  let result = -1;
+  let isFinished = false;
+  let index = 1;
+
+  if (str.length <= 1) {
+    return result;
+  }
+
+  while(!isFinished) {
+    if (index === str.length - 1) {
+      isFinished = true;
+      continue;
+    }
+
+    const test = str.slice(0, index);
+    const regex = new RegExp(test, 'g');
+    const matches = str.match(regex);
+
+    console.log(matches.join(''), str);
+
+    if (matches.length > 1 && matches.join('') === str) {
+      result = test;
+    }
+
+
+    index++;
+  }
+
+  return result;
 }
+
+console.log(StringPeriods('affedaaffed'));
 
 // Input:"abcxabc"
 // Output:"-1"
