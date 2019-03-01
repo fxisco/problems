a48a6cff0e41344981611e0a6f7809ffe80fcb8c
Francisco Payés <payesfrancisco@gmail.com>
2019-02-28 23:13:30 -0400

    (Remove brackets): init project

---
+++ removeBrackets/index.js
@@ -0,0 +1,25 @@
+/**
+ * Challenge
+ *
+ * Have the function RemoveBrackets(str) take the str string parameter being passed,
+ * which will contain only the characters "(" and ")", and determine the minimum number of
+ * brackets that need to be removed to create a string of correctly matched brackets.
+ *
+ * For example: if str is "(()))" then your program should return the number 1.
+ * The answer could potentially be 0, and there will always be at least one set
+ * of matching brackets in the string.
+ *
+ */
+
+
+function RemoveBrackets(str) {
+  return str;
+}
+
+console.log(RemoveBrackets('(()))'));
+
+// Input:"(())()((("
+// Output:3
+
+// Input:"(()("
+// Output:2
