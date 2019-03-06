d352a243a4c7ea82294428138603699e36741f61
Francisco Payés <payesfrancisco@gmail.com>
2019-03-05 22:07:37 -0400

    (Star Rating): init project

---
+++ starRating/index.js
@@ -0,0 +1,27 @@
+/**
+ * Challenge
+ *
+ * Have the function StarRating(str) take the str parameter being passed which will
+ * be an average rating between 0.00 and 5.00, and convert this rating into a list of
+ * 5 image names to be displayed in a user interface to represent the rating as a list
+ * of stars and half stars. Ratings should be rounded up to the nearest half.
+ *
+ * There are 3 image file names available: "full.jpg", "half.jpg", "empty.jpg".
+ * The output will be the name of the 5 images (without the extension), from left to right,
+ * separated by spaces. For example: if str is "2.36" then this should be displayed by the following image:
+ *
+ * So your program should return the string "full full half empty empty".
+*/
+
+function StarRating(str) {
+  return str;
+}
+
+console.log(StarRating('2.36'));
+
+// Input:"0.38"
+// Output:"half empty empty empty empty"
+
+// Input:"4.5"
+// Output:"full full full full half"
+
