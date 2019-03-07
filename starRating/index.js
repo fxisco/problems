a6427e224df47f0127381ad409898c56604dcab2
Francisco Payés <payesfrancisco@gmail.com>
2019-03-07 00:56:27 -0400

    (Star Rating): first attempt

--- starRating/index.js
+++ starRating/index.js
@@ -14,10 +14,32 @@
 */
 
 function StarRating(str) {
-  return str;
+  let response = [];
+  const stars = parseInt(str);
+  const remainingStars = parseFloat(str) % 1;
+
+  for (let i = 0; i < stars; i++) {
+    response.push('full');
+  }
+
+  if (remainingStars <= 0.25) {
+    response.push('empty');
+  } else if (remainingStars > 0.25 && remainingStars <= 0.5) {
+    response.push('half');
+  } else {
+    response.push('full');
+  }
+
+  const length = remainingStars > 0.75 ? (4 - stars) : (5 - stars);
+
+  for (let i = 0; i < length; i++) {
+    response.push('empty');
+  }
+
+  return response.join(' ');
 }
 
-console.log(StarRating('2.36'));
+console.log(StarRating('0.0'));
 
 // Input:"0.38"
 // Output:"half empty empty empty empty"
@@ -25,3 +47,12 @@ console.log(StarRating('2.36'));
 // Input:"4.5"
 // Output:"full full full full half"
 
+// Input:"3.02"
+// Output:"full full full empty empty"
+
+// Input:"2.75"
+// Output:"full full full empty empty"
+
+// 1. For the input "0.0" your output was incorrect. The correct answer is empty empty empty empty empty.
+// 2. For the input "5.0" your output was incorrect. The correct answer is full full full full full.
+// 3. For the input "4.63" your output was incorrect. The correct answer is full full full full half.
