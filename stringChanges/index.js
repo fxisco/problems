091aad1f8d3d42dbe4b504bf2418975c88a01fd8
Francisco Payés <payesfrancisco@gmail.com>
2019-03-15 01:39:48 -0400

    (String Changes): solution

--- stringChanges/index.js
+++ stringChanges/index.js
@@ -2,8 +2,9 @@
  * Challenge
  *
  * Have the function StringChanges(str) take the str parameter being passed, which will be a string containing
- * letters from the alphabet, and return a new string based on the following rules. Whenever a capital M is
- * encountered, duplicate the previous character (then remove the M), and whenever a capital N is
+ * letters from the alphabet, and return a new string based on the following rules.
+ *
+ * Whenever a capital M is encountered, duplicate the previous character (then remove the M), and whenever a capital N is
  * encountered remove the next character from the string (then remove the N). All other characters in the string
  * will be lowercase letters.
  *
@@ -13,10 +14,36 @@
  */
 
 function StringChanges(str) {
-  return str;
+  let response = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const item = str[i];
+
+    if (item === 'N') {
+      i++;
+    } else if (item === 'M') {
+      if (response.length > 0) {
+        response += response[response.length - 1];
+      }
+    } else {
+      response += item;
+    }
+  }
+
+  return response;
 }
 
-console.log(StringChanges('abcNdgM'));
+console.log(StringChanges('MNMNjMa'));
+
+// Input: MMMMMokM
+// Output: okk
+
+// Input: MMMMMokN
+// Output: ok
+
+// Input: MNMNjMa
+// Output: a
+
 
 // Input:"MrtyNNgMM"
 // Output:"rtyggg"
