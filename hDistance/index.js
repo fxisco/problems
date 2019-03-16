07d40139ff829761ed11622b8d557ffb7ef4de48
Francisco Payés <payesfrancisco@gmail.com>
2019-03-16 13:14:28 -0400

    (H Distance): init project

---
+++ hDistance/index.js
@@ -0,0 +1,23 @@
+/**
+ * Challenge
+ * Have the function HDistance(strArr) take the array of strings stored in strArr,
+ * which will only contain two strings of equal length and return the number of
+ * characters at each position that are different between them.
+ *
+ * For example: if strArr is ["house", "hours"] then your program should return 2.
+ * The string will always be of equal length and will only contain lowercase
+ * characters from the alphabet and numbers.
+ */
+
+
+function HDistance(strArr) {
+  return strArr;
+}
+
+console.log(HDistance(["house", "hours"]));
+
+// Input:["10011", "10100"]
+// Output:3
+
+// Input:["abcdef", "defabc"]
+// Output:6
