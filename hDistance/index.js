7e5f6d61ed28a4ba71ad3ee027e044150c250519
Francisco Payés <payesfrancisco@gmail.com>
2019-03-17 19:53:21 -0400

    (H Distance): solution

--- hDistance/index.js
+++ hDistance/index.js
@@ -1,5 +1,6 @@
 /**
  * Challenge
+ *
  * Have the function HDistance(strArr) take the array of strings stored in strArr,
  * which will only contain two strings of equal length and return the number of
  * characters at each position that are different between them.
@@ -11,10 +12,19 @@
 
 
 function HDistance(strArr) {
-  return strArr;
+  const [firstWord, secondWord] = strArr;
+  let response = 0;
+
+  [...firstWord].forEach((letter, index) => {
+    if (letter !== secondWord[index]) {
+      response++;
+    }
+  });
+
+  return response;
 }
 
-console.log(HDistance(["house", "hours"]));
+console.log(HDistance(["abcdef", "defabc"]));
 
 // Input:["10011", "10100"]
 // Output:3
