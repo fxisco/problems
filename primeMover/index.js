f63ffb57e778e8032e01c90908be40bba13db319
Francisco Payés <payesfrancisco@gmail.com>
2019-03-21 12:40:59 -0400

    (Prime Mover): init project

---
+++ primeMover/index.js
@@ -0,0 +1,20 @@
+/**
+ * Challenge
+ *
+ * Have the function PrimeMover(num) return the numth prime number.
+ * The range will be from 1 to 10^4.
+ *
+ * For example: if num is 16 the output should be 53 as 53 is the 16th prime number.
+ */
+
+function PrimeMover(num) {
+  return num;
+}
+
+console.log(PrimeMover(9));
+
+// Input:9
+// Output:23
+
+// Input:100
+// Output:541
