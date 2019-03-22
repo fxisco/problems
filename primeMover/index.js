78f322cfc05e0a2392069dbacd566c84d37519c1
Francisco Payés <payesfrancisco@gmail.com>
2019-03-21 22:01:43 -0400

    (Prime Mover): solution

--- primeMover/index.js
+++ primeMover/index.js
@@ -7,11 +7,42 @@
  * For example: if num is 16 the output should be 53 as 53 is the 16th prime number.
  */
 
-function PrimeMover(num) {
-  return num;
+function isPrime(num) {
+  if (num === 1) {
+    return false;
+  }
+
+  if (num === 2) {
+    return true;
+  }
+
+  const hinge = Math.floor(Math.sqrt(num));
+
+  return ![...Array(hinge)].some((_, index) => {
+    const currentNum = index + 1;
+
+    return currentNum !== 1 && currentNum !== num && num % currentNum === 0;
+  });
 }
 
-console.log(PrimeMover(9));
+function PrimeMover(num) {
+  let i = 1,
+    count = 0;
+
+  while (count < num) {
+    if (i === 2) {
+      count++;
+    } else if (i % 2 !== 0 && isPrime(i)) {
+      count++;
+    }
+
+    i++;
+  }
+
+  return i - 1;
+}
+
+console.log(PrimeMover(100));
 
 // Input:9
 // Output:23
