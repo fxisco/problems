7f16ff858b489eb4599d7e6a278fbead30f7bf5a
Francisco Payés <payesfrancisco@gmail.com>
2019-04-09 12:38:03 -0400

    (Division): init project

---
+++ division/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+/*
+	Challenge
+
+	Have the function Division(num1,num2) take both parameters being passed and return
+	the Greatest Common Factor. That is, return the greatest number that evenly goes into
+	both numbers with no remainder.
+
+	For example: 12 and 16 both are divisible by 1, 2, and 4 so the output should be 4.
+	The range for both parameters will be from 1 to 10^3.
+*/
+
+func Division(num1 int, num2 int) int {
+
+	// code goes here
+	// Note: feel free to modify the return type of this function
+	return num1
+
+}
+
+func main() {
+
+	// do not modify below here, readline is our function
+	// that properly reads in the input for you
+	fmt.Println(Division(12, 12))
+
+}
+
+// Input:7 & num2 = 3
+// Output:1
+
+// Input:36 & num2 = 54
+// Output:18
