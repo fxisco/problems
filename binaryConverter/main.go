b1a09111efc81ba2f6408af8024fb0aa8684c8af
Francisco Payés <payesfrancisco@gmail.com>
2019-04-19 17:01:37 -0400

    (Binary Converter): init project

---
+++ binaryConverter/main.go
@@ -0,0 +1,30 @@
+/*
+	Challenge
+	Have the function BinaryConverter(str) return the decimal form of the binary value. For example: if 101 is passed return 5, or if 1000 is passed return 8.
+*/
+
+package main
+
+import "fmt"
+
+func BinaryConverter(str string) string {
+
+	// code goes here
+	// Note: feel free to modify the return type of this function
+	return str
+
+}
+
+func main() {
+
+	// do not modify below here, readline is our function
+	// that properly reads in the input for you
+	fmt.Println(BinaryConverter("100101"))
+
+}
+
+// Input:"100101"
+// Output:37
+
+// Input:"011"
+// Output:3
