30285136cb2fa05d051ef4cf19791c78510fec27
Francisco Payés <payesfrancisco@gmail.com>
2019-04-21 13:04:06 -0400

    (Binary Converter): solution

--- binaryConverter/main.go
+++ binaryConverter/main.go
@@ -1,26 +1,28 @@
 /*
 	Challenge
-	Have the function BinaryConverter(str) return the decimal form of the binary value. For example: if 101 is passed return 5, or if 1000 is passed return 8.
+
+	Have the function BinaryConverter(str) return the decimal form of the binary value.
+
+	For example: if 101 is passed return 5, or if 1000 is passed return 8.
 */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func BinaryConverter(str string) string {
+	result, _ := strconv.ParseInt(str, 2, 64)
 
-	// code goes here
-	// Note: feel free to modify the return type of this function
-	return str
-
+	return strconv.FormatInt(result, 10)
 }
 
 func main() {
-
 	// do not modify below here, readline is our function
 	// that properly reads in the input for you
-	fmt.Println(BinaryConverter("100101"))
-
+	fmt.Println(BinaryConverter("011"))
 }
 
 // Input:"100101"
