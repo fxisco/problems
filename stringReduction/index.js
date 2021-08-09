006734bfac79f8e0b2fac991b542f660a0ec0967
Francisco Payés <payesfrancisco@gmail.com>
2021-08-10 01:03:52 +0200

    stringReduction: solution

---
+++ stringReduction/index.js
@@ -0,0 +1,37 @@
+const SUBSTITUTIONS = {
+  ab: "c",
+  bc: "a",
+  ac: "b",
+};
+
+function StringReduction(str) {
+  let isFinished = false;
+  let result = str.slice();
+  let index = 0;
+
+  while (!isFinished) {
+    if (index === result.length - 1) {
+      break;
+    }
+
+    if (result[index] !== result[index + 1]) {
+      const substitute = `${result[index]}${result[index + 1]}`
+        .split("")
+        .sort()
+        .join("");
+      const letter = SUBSTITUTIONS[substitute];
+
+      result = result.slice(0, index) + letter + result.slice(index + 2);
+      index = 0;
+
+      continue;
+    }
+
+    index++;
+  }
+
+  return result.length;
+}
+
+// keep this function call here
+console.log(StringReduction("cccc"));
